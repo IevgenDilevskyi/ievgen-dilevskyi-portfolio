f5a1a51e3434edd03841e6d9300867a8ca02dc65
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2021-07-19 11:28:26 -0600

    about me page + posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -25,22 +25,17 @@ export default function Post() {
   }, []);
 
   return (
-    <main className="bg-green-100 min-h-screen p-12">
+    <main className="absolute object-cover w-full h-full">
       <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive">
-          Blog Posts Page
-        </h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          {" "}
-          Welcome to my blog posts page
-        </h2>
+        <h1 className="text-5xl flex justify-center cursive"></h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12"></h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData &&
             postData.map((post, index) => (
               <article>
                 <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                   <span
-                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    className="block h-64 relative rounded shadow leading-snug bg-white border"
                     key={index}
                   >
                     <img
@@ -48,7 +43,7 @@ export default function Post() {
                       alt={post.mainImage.alt}
                       className="w-full h-full rounded-r object-cover absolute"
                     />
-                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                    <span className="relative justify items-start">
                       <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
                         {post.title}
                       </h3>
